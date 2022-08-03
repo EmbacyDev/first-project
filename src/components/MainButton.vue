6181e48e910936522946760426cc8ec50b56e970
Igor Shavlovsky <shahmanul@34gmail.com>
2022-08-03 18:14:01 +0300

    Добавил форму кнопку

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="main-button">
-    <label for="upload">
-      <input name="photo"
-             id="upload"
-             class="upload"
-             type="file"
-             multiple
-             accept="image/*,image/jpeg"
-
-      >
-    </label>
-  </div>
+  <div class="main-button"> Кнопка</div>
 </template>
 
 <script>
